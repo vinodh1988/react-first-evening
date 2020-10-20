f92676524085ca64941707ff68bc5d4e665a6550
vinodh1988 <k.c.vinodh@hotmail.com>
2020-10-20 21:40:01 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import Two from './Two';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+     render(){
+             return(
+            <div>
+                 <h1>First react component</h1>
+                 <Two/>
+                 <Two/>
+                 <Two/>
+                 <Two/>
+                 <Two/>
+                 &copy; all copy rights reserved. 2020
+             </div>
+             ) 
+     }
 }
 
 export default App;
+
---
+++ src/Two.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Two extends React.Component{
+
+     render(){
+             return(
+            <div class="box">
+                 Box box box
+             </div>
+             ) 
+     }
+}
+
+export default Two;
