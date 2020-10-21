21ba25539ea63cec92331d8d75cc687282f8e941
vinodh1988 <k.c.vinodh@hotmail.com>
2020-10-21 21:03:31 +0530

    state-and-properties

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Two from './Two';
 import './App.css';
 
 class App extends React.Component{
-
+     constructor(){
+          super();
+          this.state = {
+               appName: "My Demo react app",
+               titles : ["Products","Achievements","Team"]
+          }
+     }
      render(){
              return(
             <div>
-                 <h1>First react component</h1>
-                 <Two/>
-                 <Two/>
-                 <Two/>
-                 <Two/>
-                 <Two/>
+                 <h1>{this.state.appName}</h1>
+                
+                 <Two title={this.state.titles[0]} />
+                 <Two title={this.state.titles[1]}/>
+                 <Two title={this.state.titles[2]}/>
+                 
                  &copy; all copy rights reserved. 2020
              </div>
              ) 
